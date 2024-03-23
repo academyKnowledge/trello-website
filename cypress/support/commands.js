8b86f3ef581b58f1728dc2e2a07907e91bffc53f
academyKnowledge <academyk74@gmail.com>
2024-03-23 17:20:00 +0300

    Part1

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,16 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('loginToTrello',()=>{
+  cy.visit("/login");
+  cy.get("#user").type("razanbalatiah96@gmail.com");
+  cy.get("#login").click();
+  cy.origin("https://id.atlassian.com",()=>{
+    cy.get("#password").type("qa!@#$%^&*" +"{enter}")
+  })
+})
+
+Cypress.Commands.add('findByTestId',(testId)=>{
+    cy.get(`[data-testid=${testId}]`)
+})
